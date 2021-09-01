396d22fd0d89b68888ceb64d2336bc14f9ec531e
anny_more <anxxxen@ya.ru>
2021-09-01 21:52:24 +0300

    fix: function pass all tests

--- src/index.js
+++ src/index.js
@@ -1,66 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
   let stack = [];
-  let n = 0;
-  
-  for (let i = 0; i < str.length; i++) {
-    let j = 0;
-    let time = true;
-    while ((j < bracketsConfig.length) && time) {
+  let i = 0;
+  let j = 0;
 
-     let a = bracketsConfig[j].indexOf(str[i]);
-     let c = bracketsConfig[j].lastIndexOf(str[i]);
-     if (a !== c) {
-       console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-       n += 1;
-       j++;
-       break;
-     }
-     console.log(`let a = ${a}, j = ${j}, item = ${str[i]}`);
-    
-       switch (a) {
+  while (i < str.length && j < bracketsConfig.length) {
+    let leftIndex = bracketsConfig[j].indexOf(str[i]);
+    let rigthIndex = bracketsConfig[j].lastIndexOf(str[i]);
+        
+       switch (leftIndex) {
         case -1:
           j++;
           break;
        case 0:
-         stack.push(`${j}.${a}`);
-         console.log(`case 0: ${j}.${a}`);
-         time = false;
-         continue;
-         //break;
+         if ((rigthIndex !== leftIndex)&&(stack.length > 0)) {
+            let test = stack.pop();
+            if (test !== str[i]) {
+              stack.push(test);
+              stack.push(str[i]);
+            };
+           } else {
+             stack.push(str[i]);
+           };
+           i++;
+           j = 0;
+           break;
+
        case 1:
          if (stack.length < 1) {
            return false
-          }
-         let b = stack.pop().split('.');
-         //console.log(`d - ${d}`);
-         //let b = d.split('.');
-         console.log(`b = ${b[0]}, ${b[1]} i=${i} j=${j}`);
-         if ((b[0] == j)&&(b[1] == 0)) {
-           console.log(`true`);
-           time = false;
-           continue;
-         } else {
-           console.log(`else`);
-           time = false;
-           return false
-         }
-         //continue;
-         //break;
+          };
+         test = stack.pop();
+         let openBracket = bracketsConfig[j][0];
+         if (test !== openBracket) {
+           return false;
+         } 
+         i++;
+         j = 0;
+         break;
        }
-      
-     }
-  }
-
-  return (stack.length === 0)&&(n % 2 ===0);
+    }
+  return (stack.length === 0);
 };
-
-/*const config1 = [['(', ')']];
-const config2 = [['(', ')'], ['[', ']']];
-const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
-const config4 = [['|', '|']];
-const config5 = [['(', ')'], ['|', '|']];
-const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
-const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
-
-console.log(check('111115611111111156111111112222888888222255778777787755556666777777777766222221111222288888822225577877778775555666677777777776622222', config6));
-*/
